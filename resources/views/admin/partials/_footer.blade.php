75e017387127982f0dc69a9ad049acd41f4a7fee
sikasrafli <raflifadilah59@gmail.com>
2024-02-26 16:57:23 +0700

    first init  commit

---
+++ resources/views/admin/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+    All Rights Reserved by SMK Al Hafidz Leuwiliang. Designed and Developed by <a href="https://instagram.com/Fikri" target="_blank">Fikri Patansah</a>.
+</footer>
